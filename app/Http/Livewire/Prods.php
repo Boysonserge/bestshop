0e59e218b04dc398db72f675b618de946641365c
Boysonserge <tuyishimeserge@gmail.com>
2022-04-21 13:16:35 +0200

    Fist

---
+++ app/Http/Livewire/Prods.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Prods extends Component
+{
+    public function render()
+    {
+        return view('livewire.prods');
+    }
+}
